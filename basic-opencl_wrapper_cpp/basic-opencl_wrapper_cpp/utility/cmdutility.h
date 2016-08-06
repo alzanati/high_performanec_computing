8d84c4b6c63568b7145f0a4f99ebe1f9a4aa4a8e
alzanati <hosny1993.bio@gmail.com>
2016-08-06 23:10:54 +0200

    add color command output class

---
+++ basic-opencl_wrapper_cpp/basic-opencl_wrapper_cpp/utility/cmdutility.h
@@ -0,0 +1,32 @@
+#ifndef CMD_UTILITY_H
+#define CMD_UTILITY_H
+#include <ostream>
+
+namespace utility {
+
+namespace color {
+enum Code {
+    FG_RED      = 31,
+    FG_GREEN    = 32,
+    FG_BLUE     = 34,
+    FG_DEFAULT  = 39,
+    BG_RED      = 41,
+    BG_GREEN    = 42,
+    BG_BLUE     = 44,
+    BG_DEFAULT  = 49
+};
+
+class Modifier {
+private:
+    Code code;
+public:
+    Modifier(Code code) : code(code) {}
+    operator<<(std::ostream& os, const Modifier& mod) {
+        return os << "\033[" << mod.code << "m";
+    }
+};
+}
+
+}
+
+#endif
